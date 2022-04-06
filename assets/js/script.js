f9afae37d1e54c072b97467813893ff1731619ba
Sfzmango <maunghtike1999@gmail.com>
2022-04-05 19:23:31 -0700

    add questions wireframe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
+var promptNum = 2;
+
 // changes current box to the next box down the sibling list
 function nextPrompt() {
-    var currentBox = document.querySelector(".box");
+    var currentBox = document.body.children[promptNum];
     currentBox.style.display = "none";
     var nextBox = currentBox.nextElementSibling;
     nextBox.style.display = "block";
+    promptNum++;
+    return promptNum;
 };
 
+function 
